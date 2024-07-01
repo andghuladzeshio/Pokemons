cbd692837b0c51190d3c7acc3ac23df27eac58b6
Shio <andghuladze.shio.2@gmail.com>
2024-07-01 21:11:21 +0400

    redesigned list

--- app/presentation/src/main/java/com/example/presentation/ui/pages/pokemon_list/PokemonListScreen.kt
+++ app/presentation/src/main/java/com/example/presentation/ui/pages/pokemon_list/PokemonListScreen.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
@@ -26,12 +28,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImagePainter
@@ -60,7 +66,9 @@ fun PokemonListScreen(
     }
 
     LazyColumn(
-        modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(24.dp)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White), contentPadding = PaddingValues(24.dp)
     ) {
         items(pokemons) { pokemon ->
             PokemonView(pokemon, navController) {
@@ -102,41 +110,46 @@ fun PokemonListScreen(
 private fun PokemonView(
     pokemon: Pokemon, navController: NavHostController, onImageClick: () -> Unit
 ) {
-    Box(
+    Row(
         modifier = Modifier
+            .shadow(2.dp, RoundedCornerShape(24.dp))
             .fillMaxWidth()
+            .height(150.dp)
             .roundedCorners(24.dp)
             .background(Color.White)
             .clickable {
                 onImageClick()
-            }, contentAlignment = Alignment.BottomStart
+            }
     ) {
         ImageLoader(
             contentScale = ContentScale.FillHeight,
-            modifier = Modifier.height(150.dp),
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(16.dp),
             url = pokemon.imageUrl
         ) {
             ImageShimmer(height = 150.dp)
         }
 
-        Box(
-            Modifier
-                .fillMaxWidth()
-                .background(imageGradientBrush)
+        Spacer(modifier = Modifier.weight(1f))
+
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.End
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp, horizontal = 16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = pokemon.name, color = Color.White)
-                Spacer(modifier = Modifier.weight(1f))
-                Button(onClick = {
-                    navController.navigate(PokemonDetails(pokemon.name, pokemon.imageUrl))
-                }) {
-                    Text(text = stringResource(id = R.string.pokemons_details))
-                }
+            Text(
+                text = pokemon.name,
+                fontFamily = FontFamily.Monospace,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraLight
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Button(onClick = {
+                navController.navigate(PokemonDetails(pokemon.name, pokemon.imageUrl))
+            }) {
+                Text(text = stringResource(id = R.string.pokemons_details))
             }
         }
     }
